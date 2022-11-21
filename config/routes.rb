95210ed189da28807ae560289c0c3d2eec2b6a6a
anhnqd-ROR <nqda98@gmail.com>
2022-11-21 15:22:51 +0700

    delete

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
-  get 'users/show'
   resources :rooms do
     resources :messages
   end
   root 'pages#home'
-  devise_for :users
-  devise_scope :user do
-    get 'users', to: 'devise/sessions#new'
-  end
+  devise_for :users, controllers: {
+    sesions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get 'user/:id', to: 'users#show', as: 'user'
+  # Defines the root path route ("/")
+  # root "articles#index"
 end
