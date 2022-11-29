2b6bf5a4d90022a54c44c1b1541162396129ee79
anhnqd-ROR <118593941+anhnqd-ROR@users.noreply.github.com>
2022-11-29 14:27:01 +0700

    Revert "deploy aws"

--- config/deploy/production.rb
+++
@@ -1,5 +0,0 @@
-set :stage, :production
-set :rails_env, :production
-set :deploy_to, "/deploy/apps/turbo_chat_server"
-set :branch, :config_deploy
-server "3.112.7.151", user: "deploy", roles: %w(web app db)
